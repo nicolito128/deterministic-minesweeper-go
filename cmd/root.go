5cf2b01509a3e8bcd984b215e4fb8b5d92bdd37f
Nicolas A. Serna <serna.nicolas.abel@gmail.com>
2026-06-26 02:25:43 -0300

    up module name

---
+++ cmd/root.go
@@ -0,0 +1,37 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "minesweeper-cli",
+	Short: "A deterministic minesweeper CLI utility.",
+	Long: `You can simulate a game of minesweeper passing a seed or taking a generated seed by the program.
+	Example usage:
+		minesweeper-cli -seed 123456789 -w 12 -h 24 -act "<actions>"
+	`,
+	Run: func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringP("seed", "s", "", "Seed to fill the randomness")
+	rootCmd.Flags().StringP("actions", "a", "", "List of interactions with the current game")
+	rootCmd.Flags().Int("width", 9, "Width of the board")
+	rootCmd.Flags().Int("height", 9, "Height of the board")
+	rootCmd.Flags().IntP("mines", "n", 10, "Total amount of mines for the game")
+}
---
+++ entry/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/nicolito128/deterministic-minesweeper-go/cmd"
+
+func main() {
+	cmd.Execute()
+}
